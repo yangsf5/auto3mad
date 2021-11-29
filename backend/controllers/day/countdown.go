5cd40c0349cb0218004fb9541cf10930499ced44
shanfeng.yang <shanfeng.yang@rjoy.com>
2021-11-29 19:55:56 +0800

    CountdownController

---
+++ backend/controllers/day/countdown.go
@@ -0,0 +1,53 @@
+package day
+
+import (
+	"time"
+
+	"backend/controllers/base"
+)
+
+type CountdownController struct {
+	base.BaseController
+}
+
+type retDay struct {
+	PassedDay int `json:"passed_day"`
+	Weekend   int `json:"weekend"`
+	Holiday   int `json:"holiday"`
+	Adapter   int `json:"adapter"`
+	Ret       int `json:"ret"`
+}
+
+func (c *CountdownController) Get() {
+	loc, _ := time.LoadLocation("Asia/Shanghai")
+	start, _ := time.ParseInLocation("2006-01-02", "2021-02-25", loc)
+	now := time.Now()
+
+	diff := now.Sub(start)
+	diffDay := diff.Hours() / 24
+
+	// 计算周末天数
+	// 2021-02-25 是周四
+	// 即第一周有四天，有两个工作日，这个单独算，否则后续偏移不准确
+	// 所以先从 diffDay 里减掉第一周四天，最后再加第一周的周末两天
+	weekends := (diffDay-4)/7*2 + 2
+
+	// 国家节假日，不算周末，则每年 11 天假
+	// 即每 365 天，有 11 天假
+	holidays := diffDay * 11 / 365
+
+	// 每年给自己 10 天适配时间
+	adapter := diffDay * 10 / 365
+
+	d := diffDay - weekends - holidays - adapter
+
+	c.JSONOK(map[string]interface{}{
+		"data": retDay{
+			int(diffDay),
+			int(weekends),
+			int(holidays),
+			int(adapter),
+			int(d),
+		},
+	})
+}
--- backend/routers/router.go
+++ backend/routers/router.go
@@ -8,23 +8,11 @@
 package routers
 
 import (
-	"backend/controllers"
+	"backend/controllers/day"
 
 	beego "github.com/beego/beego/v2/server/web"
 )
 
 func init() {
-	ns := beego.NewNamespace("/v1",
-		beego.NSNamespace("/object",
-			beego.NSInclude(
-				&controllers.ObjectController{},
-			),
-		),
-		beego.NSNamespace("/user",
-			beego.NSInclude(
-				&controllers.UserController{},
-			),
-		),
-	)
-	beego.AddNamespace(ns)
+	beego.Router("/v2/day/countdowns", &day.CountdownController{})
 }
