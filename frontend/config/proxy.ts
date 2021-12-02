85bf826b2019643f5b3f9f37104cf269a5c01e7c
shanfeng.yang <shanfeng.yang@rjoy.com>
2021-12-02 23:24:22 +0800

    proxy /v2/ to backend

--- frontend/config/proxy.ts
+++ frontend/config/proxy.ts
@@ -9,12 +9,13 @@
 export default {
   dev: {
     // localhost:8000/api/** -> https://preview.pro.ant.design/api/**
-    '/api/': {
+    '/v2/': {
       // 要代理的地址
-      target: 'https://preview.pro.ant.design',
+      target: 'http://127.0.0.1:1127',
       // 配置了这个可以从 http 代理到 https
       // 依赖 origin 的功能可能需要这个，比如 cookie
       changeOrigin: true,
+      pathRewrite: { '^': '' },
     },
   },
   test: {
