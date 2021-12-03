a58599ce6a486c094a2991616348b4d7b32acfc8
shanfeng.yang <shanfeng.yang@rjoy.com>
2021-12-03 19:49:58 +0800

    move sqlconn to 3mad.conf, and ignore it

--- backend/models/db/base.go
+++ backend/models/db/base.go
@@ -2,13 +2,18 @@ package db
 
 import (
 	"github.com/beego/beego/v2/client/orm"
+	beego "github.com/beego/beego/v2/server/web"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func init() {
 	orm.RegisterDriver("mysql", orm.DRMySQL)
 
-	orm.RegisterDataBase("default", "mysql", "user:pwd@tcp(127.0.0.1:3333)/auto3mad?charset=utf8")
+	conn, err := beego.AppConfig.String("sqlconn")
+	if err != nil {
+		panic(err)
+	}
+	orm.RegisterDataBase("default", "mysql", conn)
 }
 
 func getOrm() orm.Ormer {
