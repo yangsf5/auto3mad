2de14e4954066fb7461db39e4f59b39f61b142fa
shanfeng.yang <shanfeng.yang@rjoy.com>
2021-12-23 19:57:22 +0800

    API 增加完成链接前缀，方便直接访问

--- frontend/src/pages/url/api/index.tsx
+++ frontend/src/pages/url/api/index.tsx
@@ -1,18 +1,31 @@
-import { Table } from 'antd';
+import { Table, Button } from 'antd';
 import { PageContainer } from '@ant-design/pro-layout';
 import { useRequest } from 'umi';
 import { queryApiList } from './service';
+import proxy from '../../../../config/proxy';
 
 export default () => {
   const { data } = useRequest(() => {
     return queryApiList();
   });
 
+  const target = proxy.dev['/v2/'].target;
+
   const columns = [
     {
       title: 'Router Pattern',
       dataIndex: 'router_pattern',
       key: 'router_pattern',
+    },
+    {
+      title: 'API URL',
+      dataIndex: 'router_pattern',
+      key: 'router_pattern',
+      render: (text: any) => {
+        return <Button href={`${target}${text}`} type='link' target='_blank'>
+          {target}{text}
+        </Button>;
+      }
     },
     {
       title: 'Controller',
@@ -22,7 +35,6 @@ export default () => {
   ];
 
   const list = data || [];
-  console.log(data);
 
   return (
     <PageContainer
