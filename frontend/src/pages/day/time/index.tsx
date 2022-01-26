75fd4abbc4f37deb473f9fff6e5fc138c7bc6a38
3Mad <ysf1026@gmail.com>
2022-01-26 20:51:05 +0800

    增加时间戳输入框逻辑

--- frontend/src/pages/day/time/index.tsx
+++ frontend/src/pages/day/time/index.tsx
@@ -1,13 +1,13 @@
+import { useState } from 'react';
 import { PageContainer } from '@ant-design/pro-layout';
-import { Table } from 'antd';
-import { useRequest } from 'umi';
+import ProForm, { ProFormText } from '@ant-design/pro-form';
+import { Table, Card } from 'antd';
 import { queryTimeList } from './service';
+import { TimeInfo } from './data';
 
 
 export default () => {
-  const { data } = useRequest(() => {
-    return queryTimeList(1451404800);
-  });
+  const [timeList, setTimeList] = useState<TimeInfo[]>();
 
   const columns = [
     {
@@ -33,7 +33,20 @@ export default () => {
         ],
       }}
     >
-      <Table columns={columns} dataSource={data} />
-    </PageContainer>
+      <Card>
+        <ProForm<{ timestamp: number }>
+          layout={'inline'}
+          onFinish={async (values) => {
+            const { data } = await queryTimeList(values.timestamp);
+            setTimeList(data);
+          }}
+        >
+          <ProFormText name='timestamp' label='Timestamp' />
+        </ProForm>
+      </Card>
+      <Card>
+        <Table columns={columns} dataSource={timeList} />
+      </Card>
+    </PageContainer >
   );
 };
