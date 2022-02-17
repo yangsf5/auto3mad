47747df456861cdd5f77d3bf8ad0ebced881c171
3Mad <ysf1026@gmail.com>
2022-02-17 20:31:41 +0800

    每次新重启时在 mac 右上角通知下

--- backend/main.go
+++ backend/main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	_ "backend/routers"
+	"bytes"
+	"fmt"
+	"os/exec"
 
+	"github.com/beego/beego/v2/core/logs"
 	"github.com/beego/beego/v2/server/web"
 )
 
@@ -11,6 +15,16 @@ func main() {
 		web.BConfig.WebConfig.DirectoryIndex = true
 		web.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
 	}
+
+	e := fmt.Sprintf("display notification \"Restarted.\" with title \"%s\"", web.BeeApp.Cfg.AppName)
+
+	cmd := exec.Command("osascript", "-e", e)
+	var eout bytes.Buffer
+	cmd.Stderr = &eout
+	if err := cmd.Run(); err != nil {
+		logs.Error(err)
+	}
+	logs.Error(eout.String())
 
 	web.Run()
 }
