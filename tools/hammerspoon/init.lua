ac632938738c9e1525187fc28e02dac2599c4b4f
3Mad <ysf1026@gmail.com>
2022-04-28 17:42:07 +0800

    一键窗口布局

--- tools/hammerspoon/init.lua
+++ tools/hammerspoon/init.lua
@@ -44,6 +44,33 @@ module.rightHalf = function()
 	hs.grid.set(window, cell, screen)
 end
 
+-- 坐标原点 (0,0) 为屏幕左上角
+-- (x, y) 为窗口的左上角坐标，值为比率
+-- w, h 为宽高的尺寸，值为比率
+module.moveWindowInMonitor = function(x, y, w, h)
+	local window = hs.window.focusedWindow()
+	local screen = window:screen()
+	local grid = hs.grid.getGrid(screen)
+	local cell = hs.geometry(grid.w * x, grid.h * y, grid.w * w, grid.h * h)
+	hs.grid.set(window, cell, screen)
+end
+
+-- 编码时的窗口布局
+function coding()
+  app = hs.application.find("Code") -- Visual Studio Code
+  wins = app:allWindows()
+  hs.fnutils.each(wins, function(win) win:moveToScreen(hs.screen.find("dell")) end)
+  app:setFrontmost()
+end
+
+-- 写作时的窗口布局
+function writing()
+  app = hs.application.find("语雀")
+  app:findWindow("三层目标"):moveToScreen(hs.screen.find("mi"))
+  app:findWindow("语雀"):moveToScreen(hs.screen.find("retina"))
+  app:setFrontmost()
+end
+
 local function keyBind(hyper, keyFnTable)
 	for key,fn in pairs(keyFnTable) do
 		hs.hotkey.bind(hyper, key, fn)
@@ -54,6 +81,8 @@ keyBind({}, {
 	f1 = function() module.moveWindowToMonitor("MI") end,
   f2 = function() module.moveWindowToMonitor("DELL") end,
 	f3 = function() module.moveWindowToMonitor("Retina") end,
+  f4 = coding,
+  f5 = writing,
   f10 = function() hs.audiodevice.defaultOutputDevice():setOutputMuted(true) end,
 })
 
@@ -70,6 +99,32 @@ keyBind({"rightalt"}, {
 	left = module.leftHalf,
 	right = module.rightHalf,
 })
+
+keyBind({"rightshift"}, {
+	left = function() module.moveWindowInMonitor(0, 0, 0.33, 1) end,
+  down = function() module.moveWindowInMonitor(0.33, 0, 0.33, 1) end,
+	right = function() module.moveWindowInMonitor(0.66, 0, 0.33, 1) end,
+})
+
+
+-- 监控 WIFI 变化
+function wifiChanged(watcher, message, interface)
+  wName = hs.wifi.currentNetwork()
+  print(message, " ", wName)
+end
+
+w = hs.wifi.watcher.new(wifiChanged)
+--w:watchingFor("all")
+w:start()
+
+
+-- 监控显示器拔插、分辨率等变化
+function monitorChanged()
+  all = hs.screen.allScreens()
+  hs.fnutils.each(all, function(v) print(v:name()) end)
+end
+
+hs.screen.watcher.new(monitorChanged):start()
 
 
 hs.alert.show("Hammerspoon config loaded")
