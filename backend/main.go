64eaa397b0af628cbbb83b7fef190605d7acc6e8
3Mad <ysf1026@gmail.com>
2022-05-25 22:04:31 +0800

    取消 backend 开发模式的启动通知

--- backend/main.go
+++ backend/main.go
@@ -1,14 +1,9 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
-	"os/exec"
-
 	"github.com/yangsf5/auto3mad/backend/models/db/base"
 	"github.com/yangsf5/auto3mad/backend/routers"
 
-	"github.com/beego/beego/v2/core/logs"
 	"github.com/beego/beego/v2/server/web"
 )
 
@@ -19,19 +14,6 @@ func main() {
 	}
 
 	Init()
-
-	e := fmt.Sprintf("display notification \"Restarted.\" with title \"%s\"", web.BeeApp.Cfg.AppName)
-
-	cmd := exec.Command("osascript", "-e", e)
-
-	var eout bytes.Buffer
-	cmd.Stderr = &eout
-
-	if err := cmd.Run(); err != nil {
-		logs.Error("Exec stderr", err)
-	}
-
-	logs.Debug("Exec stdout: ", eout.String())
 
 	web.Run()
 }
