2675bf58874a3308537fec9401f5d15623c1a83c
3Mad <ysf1026@gmail.com>
2022-05-27 19:14:37 +0800

    统一 SQL 文件格式：SQL 关键字全大写

--- backend/sql/auto3mad.sql
+++ backend/sql/auto3mad.sql
@@ -3,48 +3,48 @@ CREATE DATABASE auto3mad;
 USE auto3mad;
 
 CREATE TABLE `daily_time_routine` (
-	`id` int NOT NULL AUTO_INCREMENT,
-	`short_name` varchar(32) NOT NULL DEFAULT '',
-	`event_scope` varchar(256) NOT NULL,
-	`will_spend` int NOT NULL,
-	`history_spend` double DEFAULT '0',
-	`icon` varchar(64) DEFAULT '',
-	`sort` int DEFAULT NULL,
-	`object` int DEFAULT '0',
-	`object_unit` char(10) DEFAULT '',
-	`progress` int DEFAULT '0',
-	`start_date` date NOT NULL,
-	PRIMARY KEY (`id`)
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `short_name` VARCHAR(32) NOT NULL DEFAULT '',
+    `event_scope` VARCHAR(256) NOT NULL,
+    `will_spend` INT NOT NULL,
+    `history_spend` DOUBLE DEFAULT '0',
+    `icon` VARCHAR(64) DEFAULT '',
+    `sort` INT DEFAULT NULL,
+    `object` INT DEFAULT '0',
+    `object_unit` CHAR(10) DEFAULT '',
+    `progress` INT DEFAULT '0',
+    `start_date` DATE NOT NULL,
+    PRIMARY KEY (`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
 CREATE TABLE `daily_time_use` (
-	`start_time` INT unsigned NOT NULL,
-	`end_time` INT unsigned NOT NULL,
-	`routine_id` INT NOT NULL,
-	`date` DATE NOT NULL,
-	`month` CHAR(7) NOT NULL,
-	PRIMARY KEY (`start_time`)
+    `start_time` INT unsigned NOT NULL,
+    `end_time` INT unsigned NOT NULL,
+    `routine_id` INT NOT NULL,
+    `date` DATE NOT NULL,
+    `month` CHAR(7) NOT NULL,
+    PRIMARY KEY (`start_time`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
 
 CREATE TABLE `day_memorial` (
-	`id` int NOT NULL AUTO_INCREMENT,
-	`date` date NOT NULL,
-	`desc` varchar(128) NOT NULL,
-	`remind_type` int NOT NULL DEFAULT '1',
-	PRIMARY KEY (`id`)
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `date` DATE NOT NULL,
+    `desc` VARCHAR(128) NOT NULL,
+    `remind_type` INT NOT NULL DEFAULT '1',
+    PRIMARY KEY (`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
 
 CREATE TABLE `url_group` (
-	`id` int NOT NULL AUTO_INCREMENT,
-	`desc` varchar(64) NOT NULL,
-	PRIMARY KEY (`id`)
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `desc` VARCHAR(64) NOT NULL,
+    PRIMARY KEY (`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
 
 CREATE TABLE `url_item` (
-	`id` int NOT NULL AUTO_INCREMENT,
-	`icon` varchar(256) NOT NULL,
-	`url` varchar(256) NOT NULL,
-	`title` varchar(64) NOT NULL,
-	`group_id` int DEFAULT NULL,
-	PRIMARY KEY (`id`)
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `icon` VARCHAR(256) NOT NULL,
+    `url` VARCHAR(256) NOT NULL,
+    `title` VARCHAR(64) NOT NULL,
+    `group_id` INT DEFAULT NULL,
+    PRIMARY KEY (`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
